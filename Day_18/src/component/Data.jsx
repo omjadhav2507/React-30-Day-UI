7f715e27837ad34b594cfbdce4593c938cec4b6e
Om Jadhav <58546061+omjadhav2507@users.noreply.github.com>
2024-01-19 23:53:57 +0530

    Day 18 React UI component -  quiz

---
+++ Day_18/src/component/Data.jsx
@@ -0,0 +1,43 @@
+export const data = [
+    {
+      question: 'What is React?',
+      Option1: 'FrontEnd lang',
+      Option2: 'BackEnd lang',
+      Option3: 'Server lang',
+      Option4: 'Full stack lang',
+      ans: 1,
+    },
+    {
+      question: 'What does JSX stand for?',
+      Option1: 'JavaScript XML',
+      Option2: 'Java XML',
+      Option3: 'JSON XML',
+      Option4: 'JavaScript eXtension',
+      ans: 1,
+    },
+    {
+      question: 'Which of the following is a React hook?',
+      Option1: 'useState',
+      Option2: 'useEffect',
+      Option3: 'useRouter',
+      Option4: 'useDispatcher',
+      ans: 1,
+    },
+    {
+      question: 'What is the purpose of ReactDOM.render() method?',
+      Option1: 'Rendering HTML elements into the DOM',
+      Option2: 'Handling AJAX requests',
+      Option3: 'Managing state in a component',
+      Option4: 'Defining a new component',
+      ans: 1,
+    },
+    {
+      question: 'What does the virtual DOM in React do?',
+      Option1: 'Improves performance by reducing direct manipulation of the actual DOM',
+      Option2: 'Creates a copy of the entire DOM structure',
+      Option3: 'Handles routing in React applications',
+      Option4: 'Provides a virtual environment for testing',
+      ans: 1,
+    },
+  ];
+  
